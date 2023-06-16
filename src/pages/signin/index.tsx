60d4b3043b8f1ae0501c755223eff8bab932fbda
Yusuke Nakamura <yaneuranoanmaku@yahoo.co.jp>
2023-06-16 22:37:05 +0900

    implement  signin screen

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -13,16 +13,12 @@ import {
   useToast,
 } from '@chakra-ui/react'
 import { FormEvent, useState } from 'react'
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  sendEmailVerification,
-} from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { FirebaseError } from '@firebase/util'
 
 
-export const Page = () => {
 
+export const Page = () => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -31,72 +27,65 @@ export const Page = () => {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     setIsLoading(true)
     e.preventDefault()
-
     try {
       const auth = getAuth()
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      )
-      await sendEmailVerification(userCredential.user)
+      await signInWithEmailAndPassword(auth, email, password)
       setEmail('')
       setPassword('')
       toast({
-        title: 'send Email for confirmation',
+        title: 'Logged in Successfully',
         status: 'success',
         position: 'top',
       })
     } catch (e) {
-      toast({
-        title: 'One or more error occurred',
-        status: 'error',
-        position: 'top',
-      })
       if (e instanceof FirebaseError) {
+        toast({
+          title: 'Something went wrong.',
+          status: 'error',
+          position: 'top',
+        })
         console.log(e)
       }
     } finally {
-        setIsLoading(false)
+      setIsLoading(false)
     }
   }
-
   return (
     <Container py={14}>
-      <Heading>Sign up</Heading>
+      <Heading>Signin</Heading>
       <chakra.form onSubmit={handleSubmit}>
         <Spacer height={8} aria-hidden />
         <Grid gap={4}>
           <Box display={'contents'}>
             <FormControl>
               <FormLabel>Email</FormLabel>
-              <Input 
+              <Input
                 type={'email'}
                 name={'email'}
                 value={email}
                 onChange={(e) => {
-                    setEmail(e.target.value)
+                  setEmail(e.target.value)
                 }}
               />
-              </FormControl>
-              <FormControl>
-                <FormLabel>Password</FormLabel>
-                <Input 
-                  type={'password'}
-                  name={'password'}
-                  value={password}
-                  onChange={(e) => {
-                      setPassword(e.target.value)
-                  }}
-                />
-              </FormControl>
-            </Box>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
+            </FormControl>
+          </Box>
         </Grid>
-        <Spacer height={4} aria-hidden />
+        <Spacer height={4} aria-hidden/>
         <Center>
-            <Button type={'submit'} isLoading={isLoading}>
-                Create your account
-            </Button>
+          <Button type={'submit'} isLoading={isLoading}>
+            Login
+          </Button>
         </Center>
       </chakra.form>
     </Container>
