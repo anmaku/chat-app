0e41a34a6685a3c486fe845af5fb95da2dbe904d
Yusuke Nakamura <yaneuranoanmaku@yahoo.co.jp>
2023-06-16 23:30:06 +0900

    fix

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { chakra, Container, Heading } from '@chakra-ui/react'
+import { useAuthContext } from '@src/feature/auth/provider/AuthProvider'
 
 export const Header = () => {
+  const { user } = useAuthContext()
   return (
     <chakra.header py={4} bgColor={'blue.600'}>
       <Container maxW={'container.lg'}>
-        <Heading color={'white'}> Status Log out  </Heading>
+        <Heading color={'white'}>
+          { user ? 'Status: Login' : 'Status Logout' }
+        </Heading>
       </Container>
     </chakra.header>
   )
